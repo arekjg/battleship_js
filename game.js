436cd56ea43eaa6a60457a18e5b3b601746f7d0f
arekjg <arekjg@gmail.com>
2022-11-23 18:57:37 +0100

    changed status texts, changed currentPlayer variable to boolean: true-player, false-opponent

--- game.js
+++ game.js
@@ -14,39 +14,53 @@ const winConditions = [
 ];
 
 let options = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
+let isPlayerTurn = true;
 let running = false;
 
 initializeGame();
 
-function initializeGame(){
+function initializeGame()
+{
     cellsP.forEach(cell => cell.addEventListener("click", cellClicked));
     cellsO.forEach(cell => cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
+    statusText.textContent = `You start`;
     running = true;
 }
-function cellClicked(){
+function cellClicked()
+{
     const cellIndex = this.getAttribute("cellIndex");
 
-    if(!running){
+    if(!running)
+    {
         return;
     }
     updateCell(this, cellIndex);
     checkWinner();
 }
-function updateCell(cell, index){
-    options[index] = currentPlayer;
+function updateCell(cell, index)
+{
+    options[index] = isPlayerTurn;
     cell.style.backgroundColor = "#000";
 }
-function changePlayer(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    statusText.textContent = `${currentPlayer}'s turn`;
+function changePlayer()
+{
+    isPlayerTurn = (isPlayerTurn) ? false : true;
+    if(isPlayerTurn)
+    {
+        statusText.textContent = "Your turn";
+    }
+    else
+    {
+        statusText.textContent = "Wait for your turn";
+    }
 }
-function checkWinner(){
+function checkWinner()
+{
     let roundWon = false;
 
-    for(let i = 0; i < winConditions.length; i++){
+    for(let i = 0; i < winConditions.length; i++)
+    {
         const condition = winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
@@ -61,11 +75,13 @@ function checkWinner(){
         }
     }
 
-    if(roundWon){
-        statusText.textContent = `${currentPlayer} wins!`;
+    if(roundWon)
+    {
+        statusText.textContent = `You win!`;
         running = false;
     }
-    else if(!options.includes("")){
+    else if(!options.includes(""))
+    {
         statusText.textContent = "Draw!";
         running = false;
     }
@@ -73,12 +89,12 @@ function checkWinner(){
         changePlayer();
     }
 }
-function restartGame(){
-    currentPlayer = "X";
+function restartGame()
+{
+    isPlayerTurn = "P";
     options = ["", "", "", "", "", "", "", "", ""];
-    statusText.textContent = `${currentPlayer}'s turn`;
+    statusText.textContent = `You start`;
     cellsP.forEach(cell => cell.style.backgroundColor = "#FFF");
     cellsO.forEach(cell => cell.style.backgroundColor = "#FFF");
     running = true;
-
 }
